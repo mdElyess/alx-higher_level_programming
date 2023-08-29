0f522182c4aa3bb11669877bfb00bc40d72a47a8
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-08-29 19:39:42 +0100

    mandatory

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class square."""
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
